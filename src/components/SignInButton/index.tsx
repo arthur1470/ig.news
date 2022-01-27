1fb443e90e610895176b12d362ecb0fe0fc71519
Arthur Marques <arthur.l2@hotmail.com>
2022-01-27 16:48:15 -0300

    Login pelo github, usando o NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,27 @@
 import styles from './styles.module.scss';
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+import { useSession, signIn, signOut} from 'next-auth/react'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const {data:session} = useSession();    
     
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.button}>
+    return session ? (
+        <button 
+            type="button" 
+            className={styles.button}    
+            onClick={() => {signOut()}}        
+        >
             <FaGithub color="#39c758" />            
             Arthur Marques
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button type="button" className={styles.button}>
+        <button 
+            type="button" 
+            className={styles.button}
+            onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417" />            
             Sign in with GitHub
         </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton/intex';
+import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripes';
 import styles from './home.module.scss';
 
@@ -39,7 +39,7 @@ export default function Home({ product }:HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KMX3vK8VDAToZOgap02IpTJ', {
     expand: ['product']
   } );
@@ -55,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return { 
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
